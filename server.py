440c4dcd43839c0076addd0917afa3a68a02d247
DiegoAsmat12 <diegoas1205@gmail.com>
2022-01-22 19:45:05 -0500

    Limitando la cantidad de intentos y añadiendo tabla de ganadores

--- server.py
+++ server.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 app.secret_key="numero_secreto"
 
+winners = []
+
 @app.route("/", methods=["GET"])
 def showGame():
     if 'random' not in session:
-        session["random"] = random.randint(1,100)
+        session["random"] = 12
     if 'contador-intentos' not in session:
         session['contador-intentos']=0
     return render_template('index.html')
@@ -20,6 +22,8 @@ def guessNumber():
             and session["guess-message"]=="You Guessed it!"):
             return redirect("/")
         if 'contador-intentos' in session:
+            if(session['contador-intentos']>=5):
+                return redirect("/")
             session['contador-intentos']+=1
         if(guess-session["random"]>0):
             session["guess-message"]="Too High"
@@ -33,5 +37,17 @@ def guessNumber():
 def resetGame():
     session.clear()
     return redirect("/")
+
+@app.route("/register", methods=["POST"])
+def registerUser():
+    user = request.form["username"]
+    winners.append({"username":user,"attempts":session['contador-intentos']})
+    session.clear()
+    return redirect("/winners")
+
+@app.route("/winners",methods=["GET"])
+def showWinners():
+    return render_template('winnerTable.html', winners=winners)
+
 if(__name__=="__main__"):
     app.run(debug=True)
